3efc837fe864f4d63baaaf9b74b480afa984f7bb
Diogo <hi@luvsick.gg>
2025-10-03 11:25:18 +0100

    oopsie

--- Ex3/main.c
+++ Ex3/main.c
@@ -2,11 +2,11 @@
 
 int main() {
     char letra = 'A';
-    letra = 'Z';
     int ano = 2024;
     double fator = 1.123456;
     fator = 9.999;
     printf("A variavel letra tem o valor: %c\n", letra);
+    letra = 'Z';
     printf("A variavel letra tem agora o valor: %c\n", letra);
     printf("A variavel ano tem o valor: %d\n", ano);
     printf("A variavel fator tem um valor inicial: %f\n", fator);
